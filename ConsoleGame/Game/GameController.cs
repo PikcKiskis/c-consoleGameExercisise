685fb2fc57867ef77f4046971a3de9e26bb47950
AisteL <aiste.lebedeva@gmail.com>
2018-12-10 01:46:32 +0200

    namespace changed

--- ConsoleGame/Game/GameController.cs
+++ ConsoleGame/Game/GameController.cs
@@ -1,11 +1,11 @@
-﻿using Lesson9.GUI;
+﻿using ConsoleGame.Gui;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson9.Game
+namespace ConsoleGame.Game
 {
     class GameController
     {
